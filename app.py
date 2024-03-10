c50e59135d9c07fdb20c0000443d048b08fb7831
Ayoub MAAMAR <38019937+ayoubmaamar@users.noreply.github.com>
2024-03-10 13:21:43 +0100

    uop

--- app.py
+++ app.py
@@ -1,69 +1,49 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
-from paho.mqtt import client as mqtt_client
 from flask_socketio import SocketIO
-
 import paho.mqtt.client as mqtt
-import paho.mqtt.publish as publish
-from cryptography.hazmat.primitives.asymmetric import dh
-from cryptography.hazmat.primitives.serialization import ParameterFormat
-from cryptography.hazmat.primitives.serialization import Encoding
-from cryptography.hazmat.primitives.serialization import PublicFormat
-from cryptography.hazmat.primitives.asymmetric.dh import DHParameterNumbers
-from cryptography.hazmat.primitives.serialization import load_pem_public_key
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# MQTT client setup
+# MQTT setup
 mqtt_client_name = "Platform"
 mqtt_hostname = "brickgrabber135.cloud.shiftr.io"
 mqtt_port = 1883
 mqtt_user = "brickgrabber135"
+mqtt_password = '1234'  # Replace with the actual password
 
-mqtt_client = mqtt_client.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2, client_id=mqtt_client_name)
-mqtt_client.username_pw_set(username=mqtt_user, password='1234')  # Add password if necessary
+mqtt_client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2, client_id=mqtt_client_name)
+mqtt_client.username_pw_set(username=mqtt_user, password=mqtt_password)
 
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected OK Returned code=", rc)
-        client.subscribe("device/registration")
-    else:
-        print("Bad connection Returned code=", rc)
-
-def on_message(client, userdata, msg):
-    if msg.topic == "device/registration":
-        # Extract device information from the message
-        # Assuming the message payload is 'device_id,device_name'
-        device_info = msg.payload.decode().split(',')
-        device_id = device_info[0]
-        device_name = device_info[1] if len(device_info) > 1 else "Unknown"
-
-        # Insert the new device into the MongoDB collection
-        devices_collection.insert_one({'device_id': device_id, 'device_name': device_name})
-        print(f"Registered new device: {device_id} - {device_name}")
-
-mqtt_client.on_connect = on_connect
-mqtt_client.on_message = on_message
-
-mqtt_client.connect(mqtt_hostname, mqtt_port, 60)
-mqtt_client.loop_start()
-
-@socketio.on('connect')
-def test_connect():
-    print('Client connected')
-
-@socketio.on('disconnect')
-def test_disconnect():
-    print('Client disconnected')
-
-
-
-# MongoDB Atlas setup
+# MongoDB setup
 uri = "mongodb+srv://seq_user:lkwnbnZBc5EH0You@cluster0.fj9cwqh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongo_client = MongoClient(uri)
 db = mongo_client['SEQ_DB']
-devices_collection = db['devices_collection']
+devices_collection = db['devices']
+
+def on_connect(client, userdata, flags, rc, dummy):
+    print("Connected with result code " + str(rc))
+    client.subscribe("temperature-data")
+
+def on_message(client, userdata, msg):
+    if msg.topic == "temperature-data":
+        data = msg.payload.decode().split(',')
+        device_id, temperature_value = data[0], data[1] if len(data) > 1 else "Unknown"
+
+        devices_collection.update_one(
+            {'device_id': device_id}, 
+            {'$set': {'last_temperature': temperature_value}},
+            upsert=True  
+        )
+        print(f"Updated temperature for device: {device_id} - {temperature_value}")
+        
+        socketio.emit('temperature_update', {'device_id': device_id, 'temperature_value': temperature_value})
+
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
+mqtt_client.connect(mqtt_hostname, mqtt_port, 60)
+mqtt_client.loop_start()
 
 @app.route('/')
 def home():
@@ -100,6 +80,14 @@ def select_topics():
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
+@socketio.on('connect')
+def test_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
